d153428836fe04ef4f8d410b3a35ba014a423456
Romano M. Stedile <54461411+romanostd@users.noreply.github.com>
2021-09-05 14:13:34 -0300

    implementador metodos get e post

--- projeto/src/servidor.js
+++ projeto/src/servidor.js
@@ -1,15 +1,31 @@
 const porta = 3003
+const { urlencoded } = require('body-parser')
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', ( req, res, next) => {
-    console.log('chamada a função middleware')
-    next()
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45})
+    res.send(bancoDeDados.getProdutos())
     
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+    
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome, 
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
